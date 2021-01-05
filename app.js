3079f5323ac2e92766dcfe3e6bc650c686725e25
Alena Buiko <abuiko.ma@gmail.com>
2021-01-05 18:28:53 -0500

    Make navbar responsive

---
+++ app.js
@@ -0,0 +1,9 @@
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-items");
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-open");
+  });
+};
+navSlide();
