17f21ba5240bcc68499cbbbf4ae2509d6bffbe3c
Alena Buiko <abuiko.ma@gmail.com>
2021-01-09 01:55:42 -0500

    Add comments to organize sections

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const navSlide = () => {
   const burger = document.querySelector(".burger");
-  const nav = document.querySelector(".nav-items");
+  const nav = document.querySelector(".nav-list");
 
   burger.addEventListener("click", () => {
-    nav.classList.toggle("nav-open");
+    nav.classList.toggle("open");
   });
 };
 navSlide();
