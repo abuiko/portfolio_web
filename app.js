0f526ef9903885b110bc0a76806aaf24968c042e
Alena Buiko <abuiko.ma@gmail.com>
2021-01-11 22:28:14 -0500

    Add overlay fade in/out effects when click navburger

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-const navSlide = () => {
-  const burger = document.querySelector(".burger");
-  const nav = document.querySelector(".nav-list");
-
-  burger.addEventListener("click", () => {
-    nav.classList.toggle("open");
-  });
-};
-navSlide();
+const burger = document.querySelector("#btnHamburger");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+burger.addEventListener("click", () => {
+  header.classList.toggle("open");
+  if (header.classList.contains("open")) {
+    overlay.classList.remove("fade-out");
+    overlay.classList.add("fade-in");
+  } else {
+    overlay.classList.remove("fade-in");
+    overlay.classList.add("fade-out");
+  }
+});
